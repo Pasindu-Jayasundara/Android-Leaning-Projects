cf49c12d1d9c0989e6b8a5d130fc0026bf132494
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-12-10 15:43:22 +0530

    navigation

--- App3/app/src/main/java/com/example/app3/SignInActivity.java
+++ App3/app/src/main/java/com/example/app3/SignInActivity.java
@@ -1,6 +1,11 @@
 package com.example.app3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,39 @@ public class SignInActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button signInBtn = findViewById(R.id.buttonSignIn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText mobile = findViewById(R.id.editTextMobile);
+                EditText password = findViewById(R.id.editTextPassword);
+
+                String mobileText = mobile.getText().toString();
+                String passwordText = password.getText().toString();
+
+                if(mobileText.isEmpty() || passwordText.isEmpty()){
+
+                    mobile.setError("Field can not be empty");
+                    password.setError("Field can not be empty");
+
+                    Log.w("Login","Missing Information");
+                }else{
+
+                    if(mobileText.equals("0740211671") && passwordText.equals("12345678")){
+                        Log.i("Login","Success");
+
+                        Intent i = new Intent(SignInActivity.this,HomeActivity.class);
+                        startActivity(i);
+                    }else{
+                        Log.e("Login","Failed");
+                    }
+
+                }
+
+            }
+        });
+
     }
 }

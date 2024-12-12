1008bbe3903f8698af00ec580500d39f6b149625
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-12-12 11:55:51 +0530

    frame layout

--- App5/app/src/main/java/com/example/app5/MainActivity.java
+++ App5/app/src/main/java/com/example/app5/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app5;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button1 = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        button2.setVisibility(View.INVISIBLE);
+
+        button1.setOnClickListener(view -> {
+//            button2.setVisibility(View.VISIBLE);
+//            button1.setVisibility(View.INVISIBLE);
+
+            button2.bringToFront();
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                button1.setVisibility(View.VISIBLE);
+//                button2.setVisibility(View.INVISIBLE);
+
+                button1.bringToFront();
+            }
         });
     }
 }

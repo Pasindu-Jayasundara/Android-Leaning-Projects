575ef6cafd5251f8f67ce0f0b1c6ccd1eb193e85
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-12-12 16:12:16 +0530

    add custom view within another view

---
+++ App5/app/src/main/java/com/example/app5/MainActivity5.java
@@ -0,0 +1,41 @@
+package com.example.app5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main5);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn = findViewById(R.id.button24);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                View view1 = getLayoutInflater().inflate(R.layout.x,null);
+
+                LinearLayout linearLayout = findViewById(R.id.linearLayout1);
+                linearLayout.addView(view1);
+
+            }
+        });
+    }
+}

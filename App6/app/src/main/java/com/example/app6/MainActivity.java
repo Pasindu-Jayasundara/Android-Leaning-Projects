fc3beaad04ecf8f94de080b0c30ab2bba6ffb5b7
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-12-13 11:09:56 +0530

    app icon

--- App6/app/src/main/java/com/example/app6/MainActivity.java
+++ App6/app/src/main/java/com/example/app6/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app6;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Button newBtn = new Button(getApplicationContext());
+                newBtn.setText(R.string.button3);
+
+                LinearLayout ll = findViewById(R.id.linearLayout);
+                ll.addView(newBtn);
+            }
+        });
+
+    }
+
+
+    public void m(View view){
+        Toast.makeText(MainActivity.this, "Hi", Toast.LENGTH_SHORT).show();
     }
 }

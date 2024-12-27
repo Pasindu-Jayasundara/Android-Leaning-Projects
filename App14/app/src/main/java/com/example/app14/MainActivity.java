c2d7304ec8e6de5b7f09cbe380820c276b3600cb
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-12-27 10:34:26 +0530

    http connection

--- App14/app/src/main/java/com/example/app14/MainActivity.java
+++ App14/app/src/main/java/com/example/app14/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.app14;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,32 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // "http://127.0.0.1/AndroidBackend/GetRequestTest"
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("app 14 : request btn", "sending http get request");
+
+                String url = "http://127.0.0.1/AndroidBackend/GetRequestTest";
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    String responseText = response.body().string();
+
+                    Log.i("app 14 : responseText", responseText);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
         });
     }
 }

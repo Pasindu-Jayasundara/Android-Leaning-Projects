142381ae1b482a1cecc085f52575da9fdac52132
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-12-30 11:09:34 +0530

    sqlite database

--- App15/app/src/main/java/com/example/app15/MainActivity.java
+++ App15/app/src/main/java/com/example/app15/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.app15;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +25,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                A a = new A(MainActivity.this,"app15.db",null,1);
+
+
+            }
+        });
+    }
+}
+
+class A extends SQLiteOpenHelper{
+
+    public A(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE user (\n" +
+                "    id     INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "    name   TEXT    NOT NULL,\n" +
+                "    mobile TEXT    NOT NULL,\n" +
+                "    city   TEXT    NOT NULL\n" +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }

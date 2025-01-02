613d1a92dcd0b740641ed5f1ee29de29c2a00b01
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-02 14:47:57 +0530

    practicle - note taking app

--- App17/app/src/main/java/com/example/app17/CreateNoteActivity.java
+++ App17/app/src/main/java/com/example/app17/CreateNoteActivity.java
@@ -1,6 +1,9 @@
 package com.example.app17;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +14,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.app17.model.SQLiteHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
@@ -39,7 +47,45 @@ public class CreateNoteActivity extends AppCompatActivity {
                     Toast.makeText(CreateNoteActivity.this, "Please Enter Content", Toast.LENGTH_SHORT).show();
                 }else{
 
+                    SQLiteHelper sqLiteHelper = new SQLiteHelper(
+                            CreateNoteActivity.this,
+                            "mynotebook.db",
+                            null,
+                            1
+                    );
 
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            SQLiteDatabase writableDatabase = sqLiteHelper.getWritableDatabase();
+                            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
+
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put("title", editText1.getText().toString());
+                            contentValues.put("content", editText2.getText().toString());
+                            contentValues.put("date_created", format.format(new Date()));
+
+                            long id = writableDatabase.insert("notes", null, contentValues);
+                            Log.i("NotebookLog","Note Created with ID:"+String.valueOf(id));
+
+                            writableDatabase.close();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    editText1.setText("");
+                                    editText2.setText("");
+
+                                    editText1.requestFocus();
+
+                                    Toast.makeText(CreateNoteActivity.this, "Note Created", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+                    }).start();
 
                 }
             }
--- App17/app/src/main/java/com/example/app17/MainActivity.java
+++ App17/app/src/main/java/com/example/app17/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.app17;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,4 +40,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+}
+
+class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteViewHolder> {
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleView;
+        TextView contentView;
+        TextView date_createdView;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleView = itemView.findViewById(R.id.textViewTitle);
+            contentView = itemView.findViewById(R.id.textViewContent);
+            date_createdView = itemView.findViewById(R.id.textViewDate);
+        }
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View inflatedView = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item,parent,false);
+        NoteViewHolder noteViewHolder = new NoteViewHolder(inflatedView);
+
+        return noteViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }

3e12bbd6105c848c80cfcd898d5964ec2a1bbf27
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-09 03:45:11 +0530

    animation

--- App20/app/src/main/java/com/example/app20/MainActivity.java
+++ App20/app/src/main/java/com/example/app20/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.app20;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.FlingAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,52 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ImageView imageView = findViewById(R.id.imageView);
+                SpringAnimation springAnimation = new SpringAnimation(imageView, DynamicAnimation.TRANSLATION_Y);
+
+                SpringForce springForce = new SpringForce();
+                springForce.setStiffness(SpringForce.STIFFNESS_LOW);
+                springForce.setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY);
+                springForce.setFinalPosition(1000f);
+
+                springAnimation.setSpring(springForce);
+                springAnimation.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+                    @Override
+                    public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                        //Toast.makeText(MainActivity.this,"End",Toast.LENGTH_LONG).show();
+                    }
+                });
+                springAnimation.addUpdateListener(new DynamicAnimation.OnAnimationUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(DynamicAnimation animation, float value, float velocity) {
+                        //Toast.makeText(MainActivity.this,"update:"+String.valueOf(value),Toast.LENGTH_LONG).show();
+                    }
+                });
+                springAnimation.start();
+            }
+        });
+
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ImageView imageView = findViewById(R.id.imageView);
+                FlingAnimation flingAnimation = new FlingAnimation(imageView,DynamicAnimation.TRANSLATION_Y);
+
+                flingAnimation.setStartVelocity();
+                flingAnimation.setFriction(1.5f);
+
+                flingAnimation.start();
+
+            }
         });
     }
 }

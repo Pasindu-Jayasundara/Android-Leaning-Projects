5746a1dc24b465d2db6969f6530de91ac4f2a9a4
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-08 16:18:36 +0530

    practical - product registration

--- App19/app/src/main/java/com/example/app19/MainActivity.java
+++ App19/app/src/main/java/com/example/app19/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app19;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TextView titleView = findViewById(R.id.inputTextTitle);
+                TextView descriptionView = findViewById(R.id.inputTextDescription);
+                TextView categoryView = findViewById(R.id.inputTextCategory);
+                TextView brandView = findViewById(R.id.inputTextBrand);
+                TextView quantityView = findViewById(R.id.inputTextQty);
+                TextView priceView = findViewById(R.id.inputTextPrice);
+
+
+
+                Toast.makeText(MainActivity.this,"",Toast.LENGTH_LONG).show();
+
+            }
         });
     }
 }

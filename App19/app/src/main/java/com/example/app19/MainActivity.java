8d1f131177a86d4054d445337cab77f27b2e9c91
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-08 17:01:16 +0530

    practical - product registration

--- App19/app/src/main/java/com/example/app19/MainActivity.java
+++ App19/app/src/main/java/com/example/app19/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.app19;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,16 +33,74 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                TextView titleView = findViewById(R.id.inputTextTitle);
-                TextView descriptionView = findViewById(R.id.inputTextDescription);
-                TextView categoryView = findViewById(R.id.inputTextCategory);
-                TextView brandView = findViewById(R.id.inputTextBrand);
-                TextView quantityView = findViewById(R.id.inputTextQty);
-                TextView priceView = findViewById(R.id.inputTextPrice);
+                LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+                View toastView = layoutInflater.inflate(R.layout.toast_error, null,false);
 
+                Toast toast = new Toast(MainActivity.this);
+                toast.setView(toastView);
+                toast.setDuration(Toast.LENGTH_LONG);
 
+                EditText titleView = findViewById(R.id.inputTextTitle);
+                EditText descriptionView = findViewById(R.id.inputTextDescription);
+                EditText categoryView = findViewById(R.id.inputTextCategory);
+                EditText brandView = findViewById(R.id.inputTextBrand);
+                EditText quantityView = findViewById(R.id.inputTextQty);
+                EditText priceView = findViewById(R.id.inputTextPrice);
 
-                Toast.makeText(MainActivity.this,"",Toast.LENGTH_LONG).show();
+                String title = titleView.getText().toString();
+                String description = descriptionView.getText().toString();
+                String category = categoryView.getText().toString();
+                String brand = brandView.getText().toString();
+                String qty = quantityView.getText().toString();
+                String price = priceView.getText().toString();
+
+                if(title.isEmpty()){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Fill the Product Title");
+                    toast.show();
+                    return;
+                }
+
+                if(description.isEmpty()){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Fill the Product Description");
+                    toast.show();
+                    return;
+                }
+
+                if(category.isEmpty()){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Fill the Product Category");
+                    toast.show();
+                    return;
+                }
+
+                if(brand.isEmpty()){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Fill the Product Brand");
+                    toast.show();
+                    return;
+                }
+
+                if(qty.isEmpty()){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Fill the Product Quantity");
+                    toast.show();
+                    return;
+                }
+
+                if(price.isEmpty()){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Fill the Product Price");
+                    toast.show();
+                    return;
+                }
+
+                if(Integer.parseInt(qty) < 0){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Invalid Quantity");
+                    toast.show();
+                    return;
+                }
+
+                if(Integer.parseInt(price) < 0){
+                    ((TextView) toastView.findViewById(R.id.textView2)).setText("Invalid Price");
+                    toast.show();
+                    return;
+                }
 
             }
         });

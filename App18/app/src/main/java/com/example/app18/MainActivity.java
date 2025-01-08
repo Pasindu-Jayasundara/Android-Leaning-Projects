553c09cb707b89f24d950393fdbdf2d8cd396dab
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-08 10:04:20 +0530

    custom toast

--- App18/app/src/main/java/com/example/app18/MainActivity.java
+++ App18/app/src/main/java/com/example/app18/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.app18;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +28,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+                View customToastInflatedView = layoutInflater.inflate(R.layout.layout1, null, false);
+
+                TextView textView = customToastInflatedView.findViewById(R.id.textView);
+                textView.setText("Hello");
+
+                Toast toast = new Toast(MainActivity.this);
+                toast.setView(customToastInflatedView);
+                toast.setDuration(Toast.LENGTH_LONG);
+
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
+
+            }
+        });
+
+
     }
+
 }

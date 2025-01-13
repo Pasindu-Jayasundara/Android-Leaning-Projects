c487ad7f02138875b969544176fecdcf7b9bf850
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-13 11:28:51 +0530

    calendar

--- App23/app/src/main/java/com/example/app23/MainActivity.java
+++ App23/app/src/main/java/com/example/app23/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.app23;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,7 +41,32 @@ public class MainActivity extends AppCompatActivity {
         videoView.setMediaController(mediaController);
         Uri uri = Uri.parse("https://sampletestfile.com/wp-content/uploads/2023/07/1MB-MP4.mp4");
         videoView.setVideoURI(uri);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+
+            }
+        });
         videoView.start();
+
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+
+            }
+        });
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(2025, 9, 10);
+                calendarView.setDate(calendar.getTimeInMillis());
+            }
+        });
 
     }
 }

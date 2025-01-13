bf2e9ad09243f43aef927ee6909fddd9afe53ca7
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-13 12:17:16 +0530

    time picker

--- App23/app/src/main/java/com/example/app23/MainActivity2.java
+++ App23/app/src/main/java/com/example/app23/MainActivity2.java
@@ -1,12 +1,18 @@
 package com.example.app23;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -19,6 +25,26 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TimePicker timePicker = findViewById(R.id.tiePicker);
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                Log.i("abc",String.valueOf(i));
+                Log.i("abc",String.valueOf(i1));
+            }
+        });
+
+        Button btn = findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                timePicker.setHour(22);
+                timePicker.setMinute(30);
+                timePicker.invalidate();
+            }
         });
     }
 }

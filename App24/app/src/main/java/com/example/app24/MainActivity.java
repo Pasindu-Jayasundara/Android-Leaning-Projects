4252bd5f864fc6728e7ec95285d00f0233f50db3
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-14 13:33:50 +0530

    firebase

--- App24/app/src/main/java/com/example/app24/MainActivity.java
+++ App24/app/src/main/java/com/example/app24/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.app24;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +29,31 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+                HashMap<String,Object> dataHashMap = new HashMap<>();
+                dataHashMap.put("name","Pasindu");
+                dataHashMap.put("mobile","0771234567");
+                dataHashMap.put("city","Colombo");
+
+                firestore
+                        .collection("user")
+                        .add(dataHashMap)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(MainActivity.this, "Document Inserted : "+ String.valueOf(documentReference.getId()), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
         });
     }
 }

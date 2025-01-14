f7b284e9c04a7e9102d9ab7901dc7556ba1cb076
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-14 13:48:47 +0530

    firebase insert and search

--- App24/app/src/main/java/com/example/app24/MainActivity.java
+++ App24/app/src/main/java/com/example/app24/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.app24;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 
@@ -50,6 +56,33 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(DocumentReference documentReference) {
                                 Toast.makeText(MainActivity.this, "Document Inserted : "+ String.valueOf(documentReference.getId()), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                firestore
+                        .collection("user")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                if(task.isSuccessful()){
+
+                                    for (QueryDocumentSnapshot qs: task.getResult()) {
+                                        Log.i("logfb",qs.getId() + " => " + qs.getData());
+                                    }
+
+                                }
+
                             }
                         });
 

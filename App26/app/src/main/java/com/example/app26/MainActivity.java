449d061b6b92e35b603cf8906d2acce8ce9740ef
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-20 13:49:54 +0530

    search

--- App26/app/src/main/java/com/example/app26/MainActivity.java
+++ App26/app/src/main/java/com/example/app26/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,11 +19,16 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.Filter;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.Source;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -88,6 +94,92 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 }else{
                                     Log.i("TAG", "get failed with ", task.getException());
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
+        Button btn3 = findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                firestore.collection("user").document("SqDSUpAnDTZpO8uhI8WV")
+                        .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable DocumentSnapshot document, @Nullable FirebaseFirestoreException error) {
+
+                                if(document != null){
+                                    if(document.exists()){
+                                        Log.i("TAG", "notification: " + document.getData());
+                                    }
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
+        Button btn4 = findViewById(R.id.button4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                firestore.collection("user")
+
+                        //.whereEqualTo("city","Colombo")
+                        //.whereEqualTo("gender","Male")
+
+                        //.where(Filter.equalTo("city","Colombo"))
+                        //.where(Filter.equalTo("gender","Male"))
+
+                        .where(
+                                Filter.and(
+                                        Filter.equalTo("city","Colombo"),
+                                        Filter.equalTo("gender","Male")
+                                )
+                        )
+
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                List<DocumentSnapshot> documentSnapshotList = task.getResult().getDocuments();
+                                for (DocumentSnapshot documentSnapshot : documentSnapshotList){
+                                    Log.i("TAG", "DocumentSnapshot data: " + documentSnapshot.getData());
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
+        Button btn5 = findViewById(R.id.button5);
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                firestore.collection("user")
+                        .whereEqualTo("city","Colombo")
+                        .orderBy("fname", Query.Direction.DESCENDING)
+                        .limit(5)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                List<DocumentSnapshot> documentSnapshotList = task.getResult().getDocuments();
+                                for(DocumentSnapshot document:documentSnapshotList){
+                                    Log.i("TAG", "DocumentSnapshot data: " + document.getData());
                                 }
 
                             }

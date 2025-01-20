bb317c28532696a261129f489de368fcf0356272
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-20 10:12:31 +0530

    search

--- App26/app/src/main/java/com/example/app26/MainActivity.java
+++ App26/app/src/main/java/com/example/app26/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.app26;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +36,65 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+                HashMap<String, Object> user = new HashMap<>();
+                user.put("fname", "Kamal");
+                user.put("lname", "Niyan");
+                user.put("gender", "Male");
+                user.put("city", "Kandy");
+                user.put("mobile", "0717900130");
+
+                firestore.collection("user").add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.i("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i("TAG", "Error adding document", e);
+                            }
+                        });
+
+            }
+        });
+
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                firestore.collection("user").document("SqDSUpAnDTZpO8uhI8WV").get(Source.DEFAULT)
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                                if(task.isSuccessful()){
+                                    DocumentSnapshot document = task.getResult();
+                                    if(document.exists()){
+                                        Log.i("TAG", "DocumentSnapshot data: " + document.getData());
+                                    }else{
+                                        Log.i("TAG", "No such document");
+                                    }
+                                }else{
+                                    Log.i("TAG", "get failed with ", task.getException());
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
     }
 }

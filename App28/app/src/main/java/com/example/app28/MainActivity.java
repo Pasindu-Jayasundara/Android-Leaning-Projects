e9bf25dc992978e929910a4418d7898cc39eae00
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-22 01:57:14 +0530

    fully custom navigation drawer

--- App28/app/src/main/java/com/example/app28/MainActivity.java
+++ App28/app/src/main/java/com/example/app28/MainActivity.java
@@ -1,12 +1,26 @@
 package com.example.app28;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.app28.navigations.ProductFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +29,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout1);
+        LinearLayout linearLayout = findViewById(R.id.linear_layout1);
+        NavigationView navigationView = findViewById(R.id.navigation_view1);
+        Toolbar toolbar = findViewById(R.id.toolbar_1);
+        FrameLayout frameLayout = findViewById(R.id.frame_layout1);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Log.i("TAG", "onNavigationItemSelected: "+ item.toString());
+
+                if(item.getItemId()==R.id.menu_item_product){
+
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.fragmentContainerView, ProductFragment.class,null)
+                            .setReorderingAllowed(true)
+                            .commit();
+
+                    toolbar.setSubtitle(item.getTitle());
+                }
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
     }
 }

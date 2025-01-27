b618ac975046352f29ce46c4016074a5f6588f82
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-01-28 02:26:11 +0530

    map

--- App29/app/build.gradle.kts
+++ App29/app/build.gradle.kts
@@ -59,5 +59,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.gms:play-services-maps:19.0.0")
-
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
--- App29/app/src/main/java/com/example/app29/MainActivity4.java
+++ App29/app/src/main/java/com/example/app29/MainActivity4.java
@@ -1,10 +1,13 @@
 package com.example.app29;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -27,15 +30,39 @@ public class MainActivity4 extends AppCompatActivity {
         });
 
         SupportMapFragment supportMapFragment = new SupportMapFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout4,supportMapFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout4, supportMapFragment).commit();
 
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
 
+                if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{"android.Manifest.permission.ACCESS_FINE_LOCATION"}, 100);
+
+                }else{
+                    if (checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{"android.Manifest.permission.ACCESS_COARSE_LOCATION"}, 200);
+                    }else{
+                        googleMap.setMyLocationEnabled(true);
+
+                    }
+                }
 
 
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(requestCode == 100){
+                Log.i("TAG","Permission granted");
+            }else if(requestCode == 200){
+                Log.i("TAG","Permission granted");
+            }
+        }
     }
 }
